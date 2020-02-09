f5930cbe502be56d3d5566372f5c53d2d609ca3c
Florian Preinfalk <preinfalkflorian@gmail.com>
2020-02-09 19:53:04 +0100

    Insert version into plugin description

--- src/mvvwb-tiles.php
+++ src/mvvwb-tiles.php
@@ -5,7 +5,7 @@
 /**
  * Plugin Name: MVVWB-Tiles
  * Description: Plugin, which allows the user to display posts and links in a grid.
- * Version: 1.0.0
+ * Version: {{version}}
  * Author: Florian Preinfalk
  * Author URI: http://www.preinfalk.co.at
  */
